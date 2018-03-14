d28c6002714f11c9ee1c3228288dda536c758cbb
zxming <451867557@qq.com>
2018-03-14 16:11:06 +0800

    第一次提交

---
+++ mell-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,46 @@
+package cn.e3mall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.common.pojo.DataGridResult;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+
+/**
+ * 商品管理Controller
+ * <p>Title: ItemController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable long itemId) {
+		TbItem item = itemService.getItemById(itemId);
+		return item;
+	}
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public DataGridResult getItemList(int page, int rows) {
+		DataGridResult result = itemService.getItemListDataGrid(page, rows);
+		return result;
+	}
+	
+	@RequestMapping("aaa")
+	public String aaa(){
+		
+		return null;
+	}
+}
